150850fdaa63f0abb496ea62ecc3fddd5fb436ef
Mario Saldinger <mariosaldinger@gmail.com>
2013-10-20 14:51:13 +0300

    NodeJS serves index.html by default on /

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 
 express()
+  .get('/', function(request, response) {
+    response.sendfile('src/static/index.html');
+  })
   .use(express.directory('src/static'))
   .use(express.static('src/static'))
   .listen(process.env.PORT || 5000);
